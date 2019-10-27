5aec81835c12bce4e23274f427d4bf920627fd0f
EstevanBR <estevan@bluerocket.us>
2019-10-26 22:35:27 -0700

    add question cell page

---
+++ automation/pages/question_cell_page.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+from page.page import Page
+
+
+class QuestionCellPage(Page):
+    root: str = "QuestionCell.view"
+    questionLabel: str = "QuestionCell.questionLabel"
+    playButton: str = "QuestionCell.playButton"
+
+    def tap(self) -> QuestionsPage:
+        from pages.questions_page import QuestionsPage
+        self.element.click()
+        return QuestionsPage()
+
+    def tap_play_button(self) -> QuestionsPage:
+        from pages.questions_page import QuestionsPage
+        self.driver.find_element_by_accessibility_id(self.playButton).click()
+        return QuestionsPage()
