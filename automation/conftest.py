68081db8b2088f41349cdfcd837db294ece75f7c
EstevanBR <estevan@bluerocket.us>
2019-11-06 23:36:47 -0800

    just some cleanup

--- automation/conftest.py
+++ automation/conftest.py
@@ -1,14 +1,12 @@
-import pytest
+pytest_plugins = [
+    "fixtures.desired_capabilities",
+    "fixtures.driver",
+    "fixtures.mock",
+    "fixtures.page",
+    "fixtures.report",
+    "fixtures.video"
+]
 
-from fixtures.desired_capabilities import (app, bundleId, derivedDataPath,
-                                           desired_capabilities, deviceName,
-                                           fullReset, noReset, platformName,
-                                           platformVersion, udid)
-from fixtures.driver import driver
-from fixtures.mock import mock
-from fixtures.page import page
-from fixtures.report import report
-from fixtures.video import video
 
 def pytest_addoption(parser):
     parser.addoption("--mock", dest="mock", action="store_true")
